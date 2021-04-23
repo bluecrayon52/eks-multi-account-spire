ea2e06c834d5c94c9d00e8bc56dce7e114cb3f1b
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2021-04-22 23:57:43 -0700

    consolidated app mesh setup script and added cleanup script

--- helper-scripts/cleanup.sh
+++ helper-scripts/cleanup.sh
@@ -2,4 +2,44 @@
 
 set -e
 
-echo "Deleting Stuff..."
+for PROFILE in frontend backend shared
+    do 
+        if [ "$PROFILE" == "shared" ]; then 
+            kubectl config use-context $SHARED_CXT
+        elif [ "$PROFILE" == "frontend" ]; then 
+            kubectl config use-context $FRONT_CXT
+        else 
+            kubectl config use-context $BACK_CXT
+        fi
+
+        echo "Deleting the App Mesh am-multi-account-mesh..."
+        kubectl delete mesh am-multi-account-mesh
+
+        if [ "$PROFILE" == "shared" ]; then 
+            echo "Deleting the mesh-share RAM resource share..."
+            aws --profile shared ram delete-resource-share \
+              --resource-share-arn $(aws --profile shared ram get-resource-shares \
+              --resource-owner SELF | jq -r '.resourceShares[] | select((.name=="mesh-share") and (.status=="ACTIVE")).resourceShareArn') > /dev/null
+        fi 
+
+        echo "Deleting the appmesh-controller..."
+        helm -n appmesh-system delete appmesh-controller
+
+        echo "Deleting the App Mesh custom resource definitions..."
+        for i in $(kubectl get crd | grep appmesh | cut -d" " -f1)
+            do
+                kubectl delete crd $i
+            done
+        
+        echo "Deleting the appmesh-controller IAM service account..."
+        eksctl --profile $PROFILE delete iamserviceaccount \
+          --cluster eks-cluster-$PROFILE \
+          --namespace appmesh-system \
+          --name appmesh-controller
+
+        echo "Deleting the appmesh-system namespace..."
+        kubectl delete namespace appmesh-system
+
+        echo "Deleting the yelb namespace..."
+        kubectl delete ns yelb
+    done 
